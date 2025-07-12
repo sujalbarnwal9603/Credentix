2baecf4cbfc52cae585edcd762e865f336efbf00
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-07-12 22:15:57 +0530

    role model

---
+++ backend/src/models/Role.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const roleSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        enum: ["admin", "user", "editor"], // Extend as needed
+    },
+    permissions: {
+        type: [String], // e.g., ["create:user", "read:tenant"]
+        default: [],
+    },
+}, {
+    timestamps: true,
+});
+
+export const Role= mongoose.model("Role", roleSchema);
