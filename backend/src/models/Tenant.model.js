43faeb2e307490773b8f1c7ee10b1bd7004202c6
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-07-13 11:55:58 +0530

    tenant model

---
+++ backend/src/models/Tenant.model.js
@@ -0,0 +1,24 @@
+import mongoose,{Schema} from "mongoose";
+
+const tenantSchema= new Schema({
+    name:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    slug:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    createdBy:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: false, // Admin User who created the tenant
+    }
+},{timestamps: true});
+
+
+export const Tenant =mongoose.model("Tenant", tenantSchema);
