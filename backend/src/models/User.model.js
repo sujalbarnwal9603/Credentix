b72122dff431c364c0d02871a9756f4f1a1d8dd2
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-07-13 12:13:40 +0530

    security for user model

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     fullName:{
@@ -42,5 +44,42 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true});
 
+
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")) return next(); // Only hash the password if it has been modified (or is new)
+    this.password=await bcrypt.hash(this.password, 10); // hash the password with a cost factor of 10
+    next();
+})
+
+userSchema.methods.isPasswordCorrect= async function(password) {
+    return await bcrypt.compare(password, this.password); // Compare the provided password with the hashed password
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id:this._id,
+            email: this.email,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: "1d", // Access token valid for 1 day
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id:this._id,
+            email: this.email,
+            fullName: this.fullName,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: "30d", // Access token valid for 30 days
+        }
+    )
+}
 
 export const User= mongoose.model("User", userSchema);
