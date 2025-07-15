1ec57acef5fd08faf5ac13876bd568cc2553684b
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-07-15 20:10:40 +0530

    configure app.sj

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
 
 const app=express();
+
+
+app.use(helmet());
+
+
+app.use(morgan('dev'));
+
+
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN|| 'http://localhost:3000',
@@ -20,7 +32,21 @@ app.get('/', (req,res)=>{
     res.send("Your API is Live 🔥\n Welcome to Credentix Backend");
 })
 
+// ========== Routes ==========
+import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
+import roleRoutes from './routes/role.routes.js';
+import tenantRoutes from './routes/tenant.routes.js';
 
+
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/roles', roleRoutes);
+app.use('/api/v1/tenants', tenantRoutes);
+
+
+
+// ========== 404 Handler ==========
 
 app.use((req, res, next) => {
     res.status(404).json({
@@ -31,13 +57,16 @@ app.use((req, res, next) => {
 
 
 
+// ========== Error Handler ==========
 app.use((err, req, res, next) => {
     const statusCode=err.statusCode || 500;
     return res.status(statusCode).json({
         success: false,
         message: err.message || 'Internal Server Error',
+        errors: err.errors || [],
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
     }); 
-    console.error(err.stack);
+    
 
 })
 
