b0da9884903ffc1535c04982162631d74b549760
sujalbarnwal9603 <sujalbarnwal9603@gmail.com>
2025-07-18 21:29:14 +0530

    client model

---
+++ backend/src/models/Client.model.js
@@ -0,0 +1,32 @@
+import mongoose, {Schema} from "mongoose";
+
+// Define schema for 3rd party-apps using Credentix
+
+const clientSchema=new Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    client_id:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    client_secret:{
+        type:String,
+        required: true,
+    },
+    redirect_uris:{
+        type:[String], // List of allowed redirect URIs
+        required: true, 
+    },
+    createdBY:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    }
+}, {timestamps:true});
+
+
+export const Client = mongoose.model("Client", clientSchema);
